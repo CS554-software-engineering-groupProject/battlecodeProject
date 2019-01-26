1e78aa273856aeee51d12c7189f4c9561b65928d
LBIV4 <lanebartoniv@gmail.com>
2019-01-26 12:28:06 -0800

    Initial mining methods for pilgrim plus some unit tests

--- bots/psuteam7bot/pilgrim.js
+++ bots/psuteam7bot/pilgrim.js
@@ -7,5 +7,35 @@ pilgrim.doAction = (self) => {
     return;
 }
 
+/**
+ * Function to execute mining
+ */
+pilgrim.mine = (self) => {
+    const onKarboniteDepot = self.karbonite_map[self.me.y][self.me.x];
+    const onFuelDepot = self.fuel_map[self.me.y][self.me.x];
+    const maxKarbonite = SPECS.UNITS[SPECS.PILGRIM].KARBONITE_CAPACITY;
+    const maxFuel = SPECS.UNITS[SPECS.PILGRIM].FUEL_CAPACITY;
+    //Just in case you don't have one fuel
+    if(self.fuel <= 0) {
+        self.log("pilgrim " + self.id + " attempting to mine when not enough fuel");
+        return;
+    }
+    if (!onKarboniteDepot && !onFuelDepot) {
+        self.log("pilgrim " + self.id + " attempting to mine where there is no resource");
+        return;
+    } else {
+        if(onKarboniteDepot && self.me.karbonite >= maxKarbonite) {
+            self.log("pilgrim " + self.id + " attempting to mine karbonite when at capacity");
+            return;
+        } else if(onFuelDepot && self.me.fuel >= maxFuel) {
+            self.log("pilgrim " + self.id + " attempting to mine fuel when at capacity");
+            return;
+        } else {
+            return self.mine();
+        }
+    }
+}
+
+
 
 export default pilgrim;
---
+++ test/pilgrim.unittests.js
@@ -0,0 +1,108 @@
+const mocha = require('mocha');
+const chai = require('chai');
+//const BCAR = require('../projectUtils/psuteam7botCompiled.js').BCAbstractRobot;
+const MyRobot = require('../projectUtils/psuteam7botCompiled.js').MyRobot;
+const pilgrim = require('../projectUtils/psuteam7botCompiled.js').pilgrim;
+const expect = chai.expect;
+
+
+describe('Pilgrim Unit Tests', function() {
+    describe('Mining tests', function() {
+        it('should be able to mine if on a resource depot', function(done) {
+            let returnValue;
+            const myBot = new MyRobot();
+            myBot.karbonite = 100;
+            myBot.fuel = 100;
+            myBot.me = {
+                id: 1,
+                unit: 2, //Pilgrim
+                x: 0,
+                y: 0,
+                fuel: 0,
+                karbonite: 0
+            }
+
+            //Check that it can mine on karbonite depot
+            myBot.karbonite_map = [[true]];
+            myBot.fuel_map = [[false]];
+
+            returnValue = pilgrim.mine(myBot);
+            //The syntax for this expectation is ugly, but this is the best check to guarantee that 'this.mine()' was called
+            expect(returnValue['action']).equals('mine');
+
+            //Second check that it can mine on fuel depot
+            myBot.karbonite_map = [[false]];
+            myBot.fuel_map = [[true]];
+
+            returnValue = pilgrim.mine(myBot);
+            expect(returnValue['action']).equals('mine');
+
+            done();
+        });
+
+        it('should not be able to mine if not on a resource depot', function(done) {
+            let returnValue;
+            let expectedErrorLog;
+            const myBot = new MyRobot();
+            myBot.karbonite = 100;
+            myBot.fuel = 100;
+            myBot.id = 1;
+            myBot.me = {
+                unit: 2, //Pilgrim
+                x: 0,
+                y: 0,
+                fuel: 0,
+                karbonite: 0
+            }
+
+            myBot.karbonite_map = [[false]];
+            myBot.fuel_map = [[false]];
+            //Can check logs to see if expected error passed. Logs are funny in that they use JSON.stringify, hence the given structure
+            expectedErrorLog = JSON.stringify("pilgrim " + myBot.id + " attempting to mine where there is no resource");
+
+            returnValue = pilgrim.mine(myBot);
+            //Should just return undefined
+            expect(returnValue).to.be.undefined;
+            expect(myBot._bc_logs).to.include(expectedErrorLog);
+
+            done();
+        });
+
+        it('should not be able to mine if at carrying capacity', function(done) {
+            let returnValue;
+            let expectedErrorLog;
+            const myBot = new MyRobot();
+            myBot.karbonite = 100;
+            myBot.fuel = 100;
+            myBot.id = 1;
+            myBot.me = {
+                unit: 2, //Pilgrim
+                x: 0,
+                y: 0,
+                fuel: 100,
+                karbonite: 20
+            }
+
+            //Check if karbonite at capacity
+            myBot.karbonite_map = [[true]];
+            myBot.fuel_map = [[false]];
+            expectedErrorLog = JSON.stringify("pilgrim " + myBot.id + " attempting to mine karbonite when at capacity")
+
+            returnValue = pilgrim.mine(myBot);
+            expect(returnValue).to.be.undefined;
+            expect(myBot._bc_logs).to.include(expectedErrorLog);
+
+            //Check if fuel at capacity
+            myBot.karbonite_map = [[false]];
+            myBot.fuel_map = [[true]];
+            expectedErrorLog = JSON.stringify("pilgrim " + myBot.id + " attempting to mine fuel when at capacity")
+
+            returnValue = pilgrim.mine(myBot);
+            expect(returnValue).to.be.undefined;
+            expect(myBot._bc_logs).to.include(expectedErrorLog);
+
+            done();
+        });
+
+    });
+});

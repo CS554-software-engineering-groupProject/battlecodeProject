35fc032f65674179494140752f83d3eec9ba4459
Anushree Naik <anushree317@gmail.com>
2019-01-29 13:16:35 -0800

    Update castle.js

--- bots/psuteam7bot/castle.js
+++ bots/psuteam7bot/castle.js
@@ -4,6 +4,7 @@ import util from './util.js';
 
 const castle = {};
 
+
 castle.takeTurn = (self) => {
     self.log('castle taking turn')
     const visible = self.getVisibleRobots();
@@ -93,5 +94,14 @@ castle.takeTurn = (self) => {
     }
 };
 
+castle.doAction = (self) => {
+    self.log("castle" + self.id + "taking turn.");
+    if(self.me.turn < 3){
+        const place = movement.directions[(3*self.me.turn)%8];
+        self.log('castle ' + self.id + ' building pilgrim at [' + (self.me.x+place.x) + ',' + (self.me.y+place.y) +']');
+        return self.buildUnit(2, place.x, place.y);
+    }
+    return;
+}
 
 export default castle;

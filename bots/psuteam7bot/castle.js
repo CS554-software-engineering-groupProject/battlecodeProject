2292cfd950f4c491a42d9a553d540ef4cf96e153
LBIV4 <lanebartoniv@gmail.com>
2019-01-30 20:01:00 -0800

    Alwyas build prophets

--- bots/psuteam7bot/castle.js
+++ bots/psuteam7bot/castle.js
@@ -13,7 +13,7 @@ castle.doAction = (self) => {
     {
         return castle.findUnitPlace(self, 'PILGRIM');
     }
-    else if(self.me.turn < 100)
+    else
     {
         //Check if there are enough resources to produce this unit.
        if(self.fuel >= SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_FUEL && self.karbonite >= SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_KARBONITE) {

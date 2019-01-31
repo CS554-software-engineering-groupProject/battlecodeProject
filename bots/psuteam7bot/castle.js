0510035ba7eeda67823c2c06d6b8d4332962bcf3
LBIV4 <lanebartoniv@gmail.com>
2019-01-30 19:19:04 -0800

    Castle bugfix

--- bots/psuteam7bot/castle.js
+++ bots/psuteam7bot/castle.js
@@ -13,27 +13,28 @@ castle.doAction = (self) => {
     {
         return castle.findUnitPlace(self, 'PILGRIM');
     }
-    elseif(self.me.turn<100)
+    else if(self.me.turn < 100)
     {
         //Check if there are enough resources to produce this unit.
-       if(self.fuel >= SPECS['PROPHET'].CONSTRUCTION_FUEL && self.karbonite >= SPECS['PROPHET'].CONSTRUCTION_KARBONITE){
+       if(self.fuel >= SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_FUEL && self.karbonite >= SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_KARBONITE) {
            return castle.findUnitPlace(self, 'PROPHET');
        }
        return;
     }
+    return;
 
 }
 
 castle.findUnitPlace = (self, unitType) => {
     //Check if any of the adjacent tile is available. Place the unit if true.
-    for(i= -1; i<= +1; i++){   
-        for(j = -1; j<= +1; j++){
+    for(let i=-1; i<= 1; i++){   
+        for(let j = -1; j<= 1; j++){
             const location = {x: (self.me.x + i), y: (self.me.y +j)} 
-           if(movement.isPassable(location, self.map, self.getVisibleRobotMap()))
-           {
-            self.log('castle ' + self.id + ' building pilgrim at [' + (self.me.x+i) + ',' + (self.me.y+j) +']'); 
-            return self.buildUnit(SPECS[unitType], i, j);       
-           }
+            if(movement.isPassable(location, self.map, self.getVisibleRobotMap()))
+            {
+                self.log('castle ' + self.id + ' building pilgrim at [' + (self.me.x+i) + ',' + (self.me.y+j) +']'); 
+                return self.buildUnit(SPECS[unitType], i, j);       
+            }
         }
     }
     return;

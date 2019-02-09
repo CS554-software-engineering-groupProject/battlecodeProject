5c86d75402471cea94f3ec7972a935d2a167ab0c
Anushree-naik <Anushree_naik@github.com>
2019-02-09 11:13:15 -0800

    checkMessage

--- bots/psuteam7bot/castle.js
+++ bots/psuteam7bot/castle.js
@@ -13,30 +13,47 @@ castle.doAction = (self) => {
     {
         return castle.findUnitPlace(self, 'PILGRIM');
     }
-    else
+    else if(self.me.turn<100)
     {
         //Check if there are enough resources to produce this unit.
-       if(self.fuel >= SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_FUEL && self.karbonite >= SPECS.UNITS[SPECS.PROPHET].CONSTRUCTION_KARBONITE) {
+       if(self.fuel >= SPECS['PROPHET'].CONSTRUCTION_FUEL && self.karbonite >= SPECS['PROPHET'].CONSTRUCTION_KARBONITE){
            return castle.findUnitPlace(self, 'PROPHET');
        }
        return;
     }
-    return;
 
 }
 
 castle.findUnitPlace = (self, unitType) => {
     //Check if any of the adjacent tile is available. Place the unit if true.
-    for(let i=-1; i<= 1; i++){   
-        for(let j = -1; j<= 1; j++){
+    for(i= -1; i<= +1; i++){   
+        for(j = -1; j<= +1; j++){
             const location = {x: (self.me.x + i), y: (self.me.y +j)} 
-            if(movement.isPassable(location, self.map, self.getVisibleRobotMap()))
-            {
-                self.log('castle ' + self.id + ' building pilgrim at [' + (self.me.x+i) + ',' + (self.me.y+j) +']'); 
-                return self.buildUnit(SPECS[unitType], i, j);       
-            }
+           if(movement.isPassable(location, self.map, self.getVisibleRobotMap()))
+           {
+            self.log('castle ' + self.id + ' building pilgrim at [' + (self.me.x+i) + ',' + (self.me.y+j) +']'); 
+            return self.buildUnit(SPECS[unitType], i, j);       
+           }
         }
     }
     return;
+}
+
+castle.findPosition = (self, friendlyCastle) => {
+    //each castle will try to locate and record the positions of the friendly castles at the start of the game
+    
+}
+
+castle.checkMessage = (self, robots) => {
+    // each castle should be able to check for messages from their friendly castles
+    for(i= -1; i<=+1; i++){
+        for(j = -1; j<=+1;j++){
+            //use of this.getVisibleRobots() to see the robots in the vicinity 
+            const visibleRobots = this.getVisibleRobotMap();
+            const value = 0;
+            self.castle_talk(value);
+            return castle.checkMessage(self, visibleRobots)
+        }
+    }
 }
 export default castle;

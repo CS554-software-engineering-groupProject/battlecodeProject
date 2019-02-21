1ebb2a049d830c9be869ac0daf8602cacfaa47ff
mukhsia <mukhsia@pdx.edu>
2019-02-21 13:25:07 -0800

    Changed to output only lcov for the test script and only generate report for coveragereport call

--- projectUtils/test_script.sh
+++ projectUtils/test_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bc19compile -f -d ./bots/psuteam7bot/ -o ./projectUtils/psuteam7botCompiled.js
 node ./projectUtils/unitTestPrep.js
-nyc --reporter=lcov --reporter=text-lcov node_modules/.bin/mocha
+nyc --reporter=lcov node_modules/.bin/mocha
--- projectUtils/wintest.bat
+++ projectUtils/wintest.bat
@@ -1,3 +1,3 @@
 call bc19compile -f -d bots\psuteam7bot -o projectUtils\psuteam7botCompiled.js
 call node projectUtils\unitTestPrep.js
-call nyc --reporter=lcov --reporter=text-lcov node_modules/.bin/mocha
+call nyc --reporter=lcov node_modules/.bin/mocha

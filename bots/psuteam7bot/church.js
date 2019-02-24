a5c9cf05b931303856cebdba3be2f7e6a675a5a1
Anushree-naik <Anushree_naik@github.com>
2019-02-23 22:40:22 -0800

    Update church.js

--- bots/psuteam7bot/church.js
+++ bots/psuteam7bot/church.js
@@ -2,6 +2,7 @@ import {BCAbstractRobot, SPECS} from 'battlecode';
 import movement from './movement.js';
 import combat from './combat.js';
 import communication from './communication.js';
+import { setFlagsFromString } from 'v8';
 
 const church = {};
 
@@ -12,7 +13,11 @@ church.doAction = (self) => {
 }
 
 church.buildFromQueue = (self) => {
-    
+    const buildNextUnit = self.churchBuildQueue[0];
+    if(self.fuel >= SPECS.UNITS[SPECS[nextBuild.unit]].CONSTRUCTION_FUEL && 
+        self.karbonite >= SPECS.UNITS[SPECS[nextBuild.unit]].CONSTRUCTION_KARBONITE){
+            
+        }
 }
 
 export default church;

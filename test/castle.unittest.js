66938afff906820047ff66a7365f6b0000879052
Anushree-naik <Anushree_naik@github.com>
2019-02-24 11:47:53 -0800

    required files in unittest

---
+++ test/castle.unittest.js
@@ -0,0 +1,5 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const MyRobot = require('../projectUtils/psuteam7botCompiled.js').MyRobot;
+const castle = require('../projectUtils/psuteam7botCompiled.js').castle;
+const expect = chai.expect;

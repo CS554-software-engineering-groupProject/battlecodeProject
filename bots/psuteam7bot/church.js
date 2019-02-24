14f1c9c5ee8fb036a3af25ebe85676922dfb0b7d
Anushree-naik <Anushree_naik@github.com>
2019-02-23 21:21:52 -0800

    Update church.js

--- bots/psuteam7bot/church.js
+++ bots/psuteam7bot/church.js
@@ -1,3 +1,7 @@
 import {BCAbstractRobot, SPECS} from 'battlecode';
+import movement from './movement.js';
+import combat from './combat.js';
+import communication from './communication.js';
+
 
 export default church;

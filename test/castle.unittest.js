f14cd0914d1d333fc43272d278e9c204fc27c536
Anushree-naik <Anushree_naik@github.com>
2019-02-24 12:20:55 -0800

    Adding unit tests

--- test/castle.unittest.js
+++ test/castle.unittest.js
@@ -3,3 +3,7 @@ const chai = require('chai');
 const MyRobot = require('../projectUtils/psuteam7botCompiled.js').MyRobot;
 const castle = require('../projectUtils/psuteam7botCompiled.js').castle;
 const expect = chai.expect;
+
+describe('Castle Helpers Unit Tests', function(){
+
+});

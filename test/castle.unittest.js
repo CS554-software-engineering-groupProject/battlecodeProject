9c9a5291a82384ef6791a1b67b7d05ad55bfc2c8
Anushree-naik <Anushree_naik@github.com>
2019-02-27 21:38:39 -0800

    Create castle.unittest.js

---
+++ test/castle.unittest.js
@@ -0,0 +1,12 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const MyRobot = require('../projectUtils/psuteam7botCompiled.js').MyRobot;
+const castle = require('../projectUtils/psuteam7botCompiled.js').castle;
+const expect = chai.expect;
+
+describe('Castle Helpers Unit Tests', function(){
+    describe('', function(done){
+        
+    });
+
+});

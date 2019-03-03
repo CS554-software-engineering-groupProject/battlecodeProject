06c5796c3cd6f5de3faf57d816e34dd8e1c48a79
Anushree-naik <Anushree_naik@github.com>
2019-03-03 12:00:28 -0800

    Update castle.js

--- bots/psuteam7bot/castle.js
+++ bots/psuteam7bot/castle.js
@@ -191,19 +191,6 @@ castle.mirrorCastle = (myLocation, fullMap) => {
 }
 
 
-
-
-
-
-/** Algorithm:
-         * If the signal is true, call this.buildUnit.
-         * After calling this method,
-         * check if the position is available 
-         * if position is available, check if the resources are available
-         * these procedures are done in method findPosition
-         * call that method 
-         *  */
-
 /** Each castle should be able to check for messages from their friendly castles 
  * Use of this.getVisibleRobots() to see the robots in the vicinity 
  * 

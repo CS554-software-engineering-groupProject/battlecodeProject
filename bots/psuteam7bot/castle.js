ef865f1aa627006bb424085c131e2c1effe2d222
Anushree-naik <Anushree_naik@github.com>
2019-03-04 20:16:04 -0800

    Update castle.js

--- bots/psuteam7bot/castle.js
+++ bots/psuteam7bot/castle.js
@@ -60,7 +60,6 @@ castle.doAction = (self) => {
  * 
  */
 castle.findUnitPlace = (self, unitType) => {
-
     for(let i = -1; i<= 1; i++){   
         for(let j = -1; j<= 1; j++){
             const location = {x: (self.me.x + i), y: (self.me.y +j)} 

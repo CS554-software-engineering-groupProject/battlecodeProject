920e9df8daad55ef4ae223a6c1e5ed8caa87ac07
Anushree-naik <Anushree_naik@github.com>
2019-03-09 11:06:34 -0800

    Update church.js

--- bots/psuteam7bot/church.js
+++ bots/psuteam7bot/church.js
@@ -25,6 +25,9 @@ church.detectClosestResources = (position, depotMap, occupiedResources) => {
     const mapSize = depotMap.length;
     let misDist = 2*Math.pow(mapSize, 2);
     let closest = { x: -1, y: -1}
+    for(let y = 0; y < mapSize; y++){
+        
+    }
 }
 
 

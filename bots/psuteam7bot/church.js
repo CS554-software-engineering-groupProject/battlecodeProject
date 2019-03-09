fc42fd9f955f288034c07157101cc798cf735f8c
Anushree-naik <Anushree_naik@github.com>
2019-03-08 22:44:54 -0800

    removed unwanted code

--- bots/psuteam7bot/church.js
+++ bots/psuteam7bot/church.js
@@ -14,35 +14,5 @@ church.doAction = (self) => {
 
 }
 
-church.buildFromQueue = (self) => {
-    const buildNextUnit = self.churchBuildQueue[0];
-    if(self.fuel >= SPECS.UNITS[SPECS[nextBuild.unit]].CONSTRUCTION_FUEL && 
-        self.karbonite >= SPECS.UNITS[SPECS[nextBuild.unit]].CONSTRUCTION_KARBONITE){
-            self.churchBuildQueue.shift();
-            self.signal(communication.positionToSignal(nextBuild, self.map), 2);
-            return church.findUnitPlace(self, buildNextUnit);
-    }
-    else{
-        self.log('cannnot build unit'+ buildNextUnit.unit +'- not enough resources');
-        return;
-    }
-}
-
-church.findUnitPlace = (self, unitType) => {
-    for(let i = -1; i<= 1; i++){
-        for(let j = -1; j<= 1; j++){
-            const location = {x: (self.me.x + i), y: (self.me.y +j)} 
-            if(movement.isPassable(location, self.map, self.getVisibleRobotMap())){
-                //Send signal starting at turn 3 so you don't overrride location communication at start
-                if(self.me.turn > 2) {
-                    self.castleTalk(SPECS[unitType]);
-                }
-
-                self.log('church ' + self.id + ' building unit ' + unitType + ' at [' + (self.me.x+i) + ',' + (self.me.y+j) +']'); 
-                return self.buildUnit(SPECS[unitType], i, j);    
-            }
-        }
-    }
-}
 
 export default church;

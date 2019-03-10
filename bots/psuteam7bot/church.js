5ce55b4d47383deaadefdd5ba3c2bd413d15be9d
Anushree-naik <Anushree_naik@github.com>
2019-03-10 13:14:10 -0700

    Update church.js

--- bots/psuteam7bot/church.js
+++ bots/psuteam7bot/church.js
@@ -50,7 +50,9 @@ church.getResourcesInRange = (location, maxDistance, resourceMap) =>{
     const targets = [];
     let currentDist;
 
-    for(let y = 0; y < resourceMap.length; ++y)
+    for(let y = 0; y < resourceMap.length; ++y){
+        for (let x = 0; x < resourceMap.length; ++x) 
+    }
 }
 
 

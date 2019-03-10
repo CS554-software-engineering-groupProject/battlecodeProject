74d594a7b25255a54969f2ed87186435d4dca9a1
Anushree-naik <Anushree_naik@github.com>
2019-03-10 03:18:09 -0700

    added church.unittest.js

---
+++ test/church.unittest.js
@@ -0,0 +1,7 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const mockBC19 = require('../projectUtils/mockGame.js');
+const MyRobot = require('../projectUtils/psuteam7botCompiled.js').MyRobot;
+const church = require('../projectUtils/psuteam7botCompiled.js').church;
+const movement = require('../projectUtils/psuteam7botCompiled.js').movement;
+const expect = chai.expect;
